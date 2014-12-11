f789a3bb3f2fed2c5db314b33c3227e05fd68642
Kelsey Judson <kelseyjudson@gmail.com>
2014-12-11 14:03:03 +1300

    Adjust example for broken case, elements spanning mutliple sections.

--- src/jquery.orderly.js
+++ src/jquery.orderly.js
@@ -1,6 +1,6 @@
-( function( $ ) {
+( function ( $ ) {
 
-    var Orderly = function( $elements, options ) {
+    var Orderly = function ( $elements, options ) {
         this.__init( $elements, options );
     };
 
@@ -10,7 +10,7 @@
 
     // -- Constructor --
 
-    p.__init = function( $elements, options ) {
+    p.__init = function ( $elements, options ) {
         this.$elements = $elements;
         this.options   = $.extend( {}, this.defaults, options );
     };
@@ -21,7 +21,7 @@
 
     // -- Event Handlers --
 
-    p.attach = function() {
+    p.attach = function () {
         if ( this.attached === undefined ) {
             $( window ).on( 'resize', this.handlers().resize );
 
@@ -29,11 +29,11 @@
         }
     };
 
-    p.handlers = function() {
+    p.handlers = function () {
         var that = this;
 
-        return { resize: function( e ) {
-            that._eachRow( function( $elements ) {
+        return { resize: function ( e ) {
+            that._eachRow( function ( $elements ) {
                 that._resizeElements( $elements );
             });
         }};
@@ -41,10 +41,10 @@
 
     // -- Private(-ish) --
 
-    p._columnCount = function() {
+    p._columnCount = function () {
         var lastPos = 0, pos = 0, count = 0;
 
-        this.$elements.each( function() {
+        this.$elements.each( function () {
             pos = $( this ).offset().left, count += 1;
 
             if ( pos <= lastPos && lastPos !== 0 ) {
@@ -57,7 +57,7 @@
         return count;
     };
 
-    p._eachRow = function( callback ) {
+    p._eachRow = function ( callback ) {
         var d = this._dimensions();
 
         for ( var start, $row, i = 0; i < d.r; i++ ) {
@@ -68,18 +68,18 @@
         }
     };
 
-    p._dimensions = function() {
+    p._dimensions = function () {
         var columns = this._columnCount()
           , rows    = Math.ceil( this.$elements.length / columns );
 
         return { c: columns, r: rows };
     };
 
-    p._resizeElements = function( $elements ) {
+    p._resizeElements = function ( $elements ) {
         var height  = this._maxHeight( $elements )
           , options = this.options;
 
-        $elements.each( function( i, element ) {
+        $elements.each( function ( i, element ) {
             $( element ).css( 'height', height );
             $( element ).trigger( 'orderly.resize', [
                 element
@@ -91,10 +91,10 @@
         });
     };
 
-    p._maxHeight = function( $elements ) {
+    p._maxHeight = function ( $elements ) {
         var value, current, max = 0, options = this.options, box;
 
-        $elements.each( function( i, element ) {
+        $elements.each( function ( i, element ) {
             $( element ).trigger( 'orderly.reset', [
                 element
               , options.resetHeight
@@ -121,7 +121,7 @@
 
     var g = {};
 
-    g._delegate = function( options ) {
+    g._delegate = function ( options ) {
         var method = '_' + ( options && options.method || 'register' )
           , m      = g[method];
 
@@ -130,7 +130,7 @@
 
     // Call for each specific collection of elements to align per row.
 
-    g._register = function( options ) {
+    g._register = function ( options ) {
         var o = new Orderly( this, options );
 
         o.attach();
@@ -140,7 +140,7 @@
     // Call on a collection of parent containers. Each direct child element
     // will be given orderly().
 
-    g._children = function( options ) {
+    g._children = function ( options ) {
         var length  = this[0].children.length
           , counter = o.counter++; // ensures unique selectors
 
@@ -149,27 +149,27 @@
         g._register.call( this, options );
     };
 
-    g._childrenData = function( counter ) {
-        this.each( function( i, element ) {
-            $( element.children ).each( function( i, child ) {
+    g._childrenData = function ( counter ) {
+        this.each( function ( i, element ) {
+            $( element.children ).each( function ( i, child ) {
                 $( child ).attr( 'data-orderly', counter + ':' + i );
             });
         });
     };
 
-    g._registerChildren = function( length, counter, options ) {
+    g._registerChildren = function ( length, counter, options ) {
         for ( var i = 0; i < length; i++ ) {
             g._register.call( g._find( counter, i ), options );
         }
     };
 
-    g._find = function( counter, i ) {
+    g._find = function ( counter, i ) {
         return $( '[data-orderly="' + counter + ':' + i + '"]' );
     };
 
     // ---
 
-    $.fn.orderly = function( options ) {
+    $.fn.orderly = function ( options ) {
         if ( this.length > 0 ) {
             g._delegate.call( this, options );
         }
